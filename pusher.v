340599a40541a59bf5011b0a60bca2ad517b1679
Nana Osei Yeboah <yeboahnanaosei@gmail.com>
2021-07-10 03:01:25 +0000

    Add initial implementation of channels endpoint

--- pusher.v
+++ pusher.v
@@ -77,27 +77,30 @@ pub mut:
 	channel string            [required]
 }
 
+// A BatchEvent is an internal representation of an event to be used in a batch request
 struct BatchEvent {
 	name string
 	data string
 	channel string
 }
 
-// trigger_batch lets you trigger mulitple Events in one call.
-// Note that you are limited to a maximum of 10 events per call
-pub fn (c Client) trigger_batch(batch []Event) ?http.Response {
-	c.is_valid() ?
-
-	mut payload := []BatchEvent{}
-
-	for e in batch {
-		payload << BatchEvent{
+fn create_batch_events(events []Event) []BatchEvent {
+	mut batch_events := []BatchEvent{}
+	for e in events {
+		batch_events << BatchEvent{
 			name: e.name
 			channel: e.channel
 			data: json.encode(e.data)
 		}
 	}
+	return batch_events
+}
 
+// trigger_batch lets you trigger mulitple Events in one call.
+// Note that you are limited to a maximum of 10 events per call
+pub fn (c Client) trigger_batch(batch []Event) ?http.Response {
+	c.is_valid() ?
+	payload := create_batch_events(batch)
 	body := json.encode(map{"batch": payload})
 	body_md5_hash := md5.hexhash(body)
 	timestamp := time.now().unix_time()
@@ -114,5 +117,59 @@ pub fn (c Client) trigger_batch(batch []Event) ?http.Response {
 
 	request.add_header(.content_type, 'application/json')
 	request.data = body
+	return request.do()
+}
+
+
+pub enum ChannelAttribute {
+	user_count
+}
+
+pub enum ChannelFilter {
+	private
+	presence
+}
+
+// channels fetches information about multiple channels
+// It allows you to fetch a hash of occupied channels (optionally filtered by prefix),
+// and optionally one or more attributes for each channel.
+pub fn (c Client) channels(prefix string, attrs string) ?http.Response {
+	c.is_valid() ?
+
+	timestamp := time.now().unix_time()
+	signature := hmac.new(
+		c.secret.bytes(),
+		'GET\n/apps/$c.app_id/channels\nauth_key=$c.key&auth_timestamp=$timestamp&auth_version=1.0&filter_by_prefix=$prefix&info=$attrs'.bytes(),
+		sha256.sum256, sha256.block_size
+	)
+
+	url := 'https://api-${c.cluster}.pusher.com/apps/$c.app_id/channels?auth_key=$c.key&auth_timestamp=$timestamp&auth_version=1.0&auth_signature=${signature.hex()}&filter_by_prefix=$prefix&info=$attrs'
+
+	mut request := http.Request{
+		method: .get
+		url: url
+	}
+
+	return request.do()
+}
+
+// channel fetches one or some attributes about a particular channel.
+pub fn (c Client) channel(channel string, attrs string) ?http.Response {
+	c.is_valid() ?
+
+	timestamp := time.now().unix_time()
+	signature := hmac.new(
+		c.secret.bytes(),
+		'GET\n/apps/$c.app_id/channels/$channel\nauth_key=$c.key&auth_timestamp=$timestamp&auth_version=1.0&info=$attrs'.bytes(),
+		sha256.sum256, sha256.block_size
+	)
+
+	url := 'https://api-${c.cluster}.pusher.com/apps/$c.app_id/channels/$channel?auth_key=$c.key&auth_timestamp=$timestamp&auth_version=1.0&auth_signature=${signature.hex()}&info=$attrs'
+
+	mut request := http.Request{
+		method: .get
+		url: url
+	}
+
 	return request.do()
 }
